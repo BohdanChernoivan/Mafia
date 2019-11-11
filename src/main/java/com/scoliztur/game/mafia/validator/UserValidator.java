87cecb4a87d9814b6ce49c6b9cfbaee609c7ba62
BohdanChernoivan <scoliztur@gmail.com>
2019-11-11 16:10:03 +0300

    validator correct

--- src/main/java/com/scoliztur/game/mafia/validator/UserValidator.java
+++ src/main/java/com/scoliztur/game/mafia/validator/UserValidator.java
@@ -1,9 +1,5 @@
 package com.scoliztur.game.mafia.validator;
 
-import com.scoliztur.game.mafia.entity.AppUser;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class UserValidator {
 
     private static final String LOGIN = "^[a-zA-Z0-9_-]{4,16}$";
@@ -13,23 +9,18 @@ public class UserValidator {
     private static final String PASSWORD = "^[a-zA-Z0-9]{5,12}$";
 
 
-    public boolean validate(AppUser appUser) {
+    public boolean validateLogin(String userLogin) {
 
-        if (!appUser.getLogin().matches(LOGIN)) {
-            log.warn("Login must be between 4 and 16 characters. {}", appUser.getLogin());
-            return false;
-        }
+        return !userLogin.matches(LOGIN);
+    }
 
-        if (!appUser.getUsername().matches(USERNAME)) {
-            log.warn("Username must be between 3 and 12 characters. {}", appUser.getUsername());
-            return false;
-        }
+    public boolean validateUsername(String userUsername) {
 
-        if (!appUser.getPassword().matches(PASSWORD)) {
-            log.warn("Password must be over 5 characters. {}", appUser.getPassword());
-            return false;
-        }
+        return !userUsername.matches(USERNAME);
+    }
 
-        return true;
+    public boolean validatePassword(String userPassword) {
+
+        return !userPassword.matches(PASSWORD);
     }
 }
